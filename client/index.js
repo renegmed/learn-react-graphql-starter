c8d3893ce751179733a107bd41b4dc1432ab70f1
rene <renegmed@yahoo.com>
2021-01-02 13:57:01 -0500

    section 13: client side setup

--- client/index.js
+++ client/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id  // caching effect
+});
 const Root = () => {
   return (
+    <ApolloProvider client={client}> 
     <div>
       Auth Starter
     </div>
+    </ApolloProvider>
   );
 };
 
