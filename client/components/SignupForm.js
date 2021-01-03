e84025a96ac359226f2df56d791f789c85fb3fab
rene <renegmed@yahoo.com>
2021-01-02 20:46:43 -0500

    section 13: the signup mutation

---
+++ client/components/SignupForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+import { graphql } from 'react-apollo';
+import mutation from '../mutations/Signup';
+class SignupForm extends Component {
+    render(){
+        return (
+        <div>
+            <h3>Sign Up</h3>
+            <AuthForm />
+        </div>
+    )};    
+}
+
+export default grahpql(mutation)(SignupForm);
